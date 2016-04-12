591676662e8d4d6f1251a828223e6f981d8f62e8
Nimrod Wandera <wanderanimrod@gmail.com>
2016-04-12 09:53:00 -0500

    Running xunit test

---
+++ Test/CellTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Test
+{
+	public class CellTest
+	{
+		[Fact]
+		public void PassingTest()
+		{
+			Assert.Equal(4, Add(2, 2));
+		}
+
+		[Fact]
+		public void FailingTest()
+		{
+			Assert.Equal(5, Add(2, 3));
+		}
+
+		int Add(int x, int y)
+		{
+			return x + y;
+		}
+	}
+}
